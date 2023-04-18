96c1cef72457e20057a66c82a7ce5393d9810ece
Mona Lisa <78098857+biellil@users.noreply.github.com>
2023-04-18 07:28:44 -0300

    18/04
    html base

--- src/pages/Dashboard/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './styles.css';
-
-export const Dashboard = ()=> {
-  return (
-    <h1>Olá Dashboard</h1>
-  )
-};
--- src/pages/Repo/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const repo = ()=> {
-  return (
-    <h1>Olá repo</h1>
-  )
-};
