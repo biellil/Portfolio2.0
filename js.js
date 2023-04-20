00c73e212f8354d3e87fda1a7e963682c1d3be3c
biel.lil <78098857+biellil@users.noreply.github.com>
2023-04-20 16:30:24 -0300

    20/04

--- js.js
+++ js.js
@@ -1,21 +1,10 @@
-// selecione todos os elementos .navbara
-const navbaras = document.querySelectorAll('.navbara');
+// JavaScript
+const menu = document.getElementById("menu");
 
-// verifique as dimensões da janela do navegador sempre que a janela for redimensionada
-window.addEventListener('resize', () => {
-  const width = window.innerWidth;
- 
-
-  // verifique se a largura e a altura atendem às suas condições
-  if (width === 425  || width <= 425 ) {
-    // itere sobre todos os elementos .navbara e oculte-os se as condições forem atendidas
-    navbaras.forEach(navbara => {
-      navbara.style.display = 'none';
-    });
+menu.addEventListener("click", function() {
+  if (menu.textContent === "menu") {
+    menu.textContent = "menu_open";
   } else {
-    // itere sobre todos os elementos .navbara e exiba-os novamente se as condições não forem atendidas
-    navbaras.forEach(navbara => {
-      navbara.style.display = '';
-    });
+    menu.textContent = "menu";
   }
 });
